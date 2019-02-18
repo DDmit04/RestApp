11dc8197052ed65e7c1ed6834b9619803a34519d
DDmit04 <danya.dmit1@gmail.com>
2019-02-18 16:28:08 +0400

    late commit

---
+++ src/main/java/letscode/sarafan/controller/MainController.java
@@ -0,0 +1,41 @@
+package letscode.sarafan.controller;
+
+import letscode.sarafan.domain.User;
+import letscode.sarafan.repo.MessageRepo;
+import lombok.Value;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	private final MessageRepo messageRepo;
+
+	@org.springframework.beans.factory.annotation.Value("${spring.profiles.active}")
+	private String profile;
+
+	@Autowired
+	public MainController(MessageRepo messageRepo) {
+		this.messageRepo = messageRepo;
+	}
+
+	@GetMapping
+	public String main(Model model, @AuthenticationPrincipal User user) {
+		HashMap<Object, Object> data = new HashMap<>();
+		if (user != null) {
+			data.put("profile", user);
+			data.put("messages", messageRepo.findAll());
+		}
+		model.addAttribute("frontendData", data);
+		model.addAttribute("isDevMode", "dev".equals(profile));
+		return "index";
+	}
+}
